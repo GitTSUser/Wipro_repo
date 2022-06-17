c506908014c945cad77a580c43e2bdf631c295bd
GitTSUser <40158247+GitTSUser@users.noreply.github.com>
2022-06-17 20:26:11 +0530

    adding app.java

---
+++ App.java
@@ -0,0 +1,18 @@
+package com.wipro.apps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	
+	public static String sayHello() {
+		return "Hello How are you";
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( sayHello() );
+    }
+}
